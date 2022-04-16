4893801e0bdb9f51d687cf69b0e3b52ca7404aea
kylee <ky.lee@catenoid.net>
2022-04-16 10:10:42 +0900

    move burrow source

--- pipeline/burrow-build.sh
+++ pipeline/burrow-build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/linkedin/Burrow
+CUR_PWD=${PWD}
+
+cd tmp
+
+# 디렉토리 존재 유무 확인
+if [ ! -d "Burrow" ] ; then
+    git clone https://github.com/linkedin/Burrow.git
+fi
 
 cd Burrow
 docker build . -t hy22-burrow:0.1
-cd ..
+cd ${CUR_PWD}
