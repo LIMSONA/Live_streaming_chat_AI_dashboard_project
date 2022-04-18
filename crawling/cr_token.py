4ea330624bcf0558f97a3aac4c6f4209035b2aaa
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2022-04-19 00:56:39 +0900

    test_finish

--- crawling/cr_token.py
+++ crawling/cr_token.py
@@ -4,16 +4,18 @@ from jamo import h2j, j2hcj #초성/중성/종성분리
 
 
 class c_token:
+
 # 1.문자만 필터링(이모티콘제외)
     def filter_str(self, input_str):
         re_string = re.compile('[^ A-Za-z0-9ㄱ-ㅣ가-힣]+').sub('', input_str)
-        return re_string
+        return print(re_string)
 
 # 2. 
 # 2-1.텍스트를 형태소 단위로 나눔
 # norm(정규화),stem(어간추출) 기본값 False
     def m_tokenize(self, input_str):
         okt = Okt()
+        # m_token = okt.morphs(self.filter_str(input_str))
         m_token = okt.morphs(self.filter_str(input_str))
         return print(m_token)
 #2-2. 초성/중성/종성분리
@@ -37,8 +39,3 @@ class c_token:
         okt = Okt()
         token = okt.nouns(self.filter_str(input_str))
         return token
-    
-
-a= "집에 가고 싶다.... ㅋㅋㅋㅋ★"
-
-c_token.m_tokenize(a)
