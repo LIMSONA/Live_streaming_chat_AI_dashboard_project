0514b810cd30514bb5bef20462911f222948090f
LIMSONA <lightsn52@gmail.com>
2022-04-18 18:05:46 +0900

    refactor_crawling_kafka

---
+++ crawling/cr_kafka.py
@@ -0,0 +1,32 @@
+from kafka import KafkaProducer, KafkaConsumer
+from json import dumps, loads
+import time
+
+kafka_server= "kafka:9092"
+
+class c_kafka: 
+    def pro_kafka(self, topic_name, get_data):
+# topic_name = "test_topic" #토픽확인! 
+        producer = KafkaProducer(
+                acks=0,
+                compression_type='gzip', #CPU 사용은 많지만 속도는 빠르니
+                bootstrap_servers=[kafka_server],
+                value_serializer=lambda x: dumps(x).encode('utf-8')
+                )
+        producer.send(topic_name, value=get_data)
+        producer.flush()
+
+
+
+
+    def con_kafka(self, topic_name):      
+        consumer = KafkaConsumer(
+                    topic_name,
+                    bootstrap_servers=[kafka_server],
+                    auto_offset_reset='earliest', # 또는 latest
+                    enable_auto_commit=True,
+                    group_id=None, #일단 그룹아이디 만들지 말고
+                    value_deserializer=lambda x: loads(x.decode('utf-8')),
+                    consumer_timeout_ms=1000
+                    )
+        return consumer    
