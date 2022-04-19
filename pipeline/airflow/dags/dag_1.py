f16c06a2591e71795a9f5fee141df23e7dbaae13
KaheePark <mydnjang@naver.com>
2022-04-19 22:24:19 +0900

    test_dag

--- pipeline/airflow/dags/dag_1.py
+++ pipeline/airflow/dags/dag_1.py
@@ -2,8 +2,11 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.dummy_operator import DummyOperator
+# DAG - dag를 정의하기 위함
+# operators - 실제 연산(실행)을 하기 위해 필요한 operators들을 불러오기 위함
 
-# args = 정해지지 않은 수의 (일반)파라미터
+# args = 정해지지 않은 수의 (일반)파라미터 
+# default_args = default로 제공하는 파라미터의 집합
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -16,7 +19,7 @@ dag = DAG(
     description = 'DAG_1_description',
     default_args= default_args,
     catchup = False,
-    schedule_interval= '*/1 * * * *') #1분마다 
+    schedule_interval= '*/1 * * * *') #1분마다 interval
 
 start = DummyOperator(
     task_id = 'DAG_1_start',
