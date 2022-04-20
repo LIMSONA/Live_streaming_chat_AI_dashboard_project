dde08b1f342f9fde660317604525d717b07a7be9
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2022-04-21 01:50:49 +0900

    test_3_in_home

--- crawling/kafka_consumer.py
+++ crawling/kafka_consumer.py
@@ -9,21 +9,35 @@ bootstrap_servers = os.getenv('KAFKA_SERVER')
 print(bootstrap_servers)
 
 # topic, broker list
-topic_name = "test_topic" #꼭 producer파일 확인하기!
-consumer = KafkaConsumer(
-            topic_name,
-            bootstrap_servers=[bootstrap_servers],
-            auto_offset_reset='latest',
-            enable_auto_commit=True,
-            group_id='my-group',
-            value_deserializer=lambda x: loads(x.decode('utf-8')),
-            consumer_timeout_ms=1000
-            )
+topic_name = "input" #꼭 producer파일 확인하기!
+# consumer = KafkaConsumer(
+#             topic_name,
+#             bootstrap_servers=[bootstrap_servers],
+#             auto_offset_reset='latest',
+#             enable_auto_commit=True,
+#             group_id='my-group',
+#             value_deserializer=lambda x: loads(x.decode('utf-8')),
+#             consumer_timeout_ms=1000
+#             )
 
 # consumer list를 가져온다
-start = time.time()
-print("[begin] Topic: %s 으로 consumer가 메시지 받아옴" % (topic_name))
-for message in consumer:
+# start = time.time()
+# print("[begin] Topic: %s 으로 consumer가 메시지 받아옴" % (topic_name))
+# for message in consumer:
     # print("Partition: %d, Offset: %d, Value: %s" % ( message.partition, message.offset,message.value ))
-    print(consumer)
 # print("[end] 걸린시간 :", time.time() - start)
+
+while True:
+    consumer = KafkaConsumer(
+                topic_name,
+                bootstrap_servers=[bootstrap_servers],
+                auto_offset_reset='latest',
+                enable_auto_commit=True,
+                group_id='my-group',
+                value_deserializer=lambda x: loads(x.decode('utf-8')),
+                consumer_timeout_ms=1000
+                )
+    print("[begin] Topic: %s 으로 consumer가 메시지 받아옴" % (topic_name))
+    # for message in consumer:
+    #     print("Partition: %d, Offset: %d, Value: %s" % ( message.partition, message.offset,message.value ))
+    print(consumer)
