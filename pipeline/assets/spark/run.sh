793579bd178f5e0e6ccf97ff9ba88df5c99342e2
EunKyung Ban <eunkyung6969@gmail.com>
2022-04-24 20:29:07 +0900

    받기받기(체리픽)

---
+++ pipeline/assets/spark/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+docker run -it \
+    --rm \
+    --name=spark-master \
+    -h spark-master \
+    -p 7077:7077 \
+    -p 8080:8080 \
+    -v ${PWD}/entrypoint.sh:/usr/local/bin/entrypoint.sh \
+    spark-hyunjinee:0.1 bash 
+
