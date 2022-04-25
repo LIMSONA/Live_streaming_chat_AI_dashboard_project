d1216814df88ca3576109aff4835f90d5cc37f46
EunKyung Ban <eunkyung6969@gmail.com>
2022-04-25 19:27:28 +0900

    final갖고온거
    Former-commit-id: 5986773ecd9acab03fe25e872183759c9262df69

--- crawling/kafka_consumer.py
+++ crawling/kafka_consumer.py
@@ -1,26 +1,19 @@
 from kafka import KafkaConsumer
-from json import loads
+import json
 import time
-import os
 
-bootstrap_servers = os.getenv('KAFKA_SERVER')
+bootstrap_servers="kafka:9092"
 
-# topic, broker list
-topic_name = "test_topic" #꼭 producer파일 확인하기!
-consumer = KafkaConsumer(
-            topic_name,
-            bootstrap_servers=[bootstrap_servers],
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='my-group',
-            value_deserializer=lambda x: loads(x.decode('utf-8')),
-            consumer_timeout_ms=1000
-            )
 
-# consumer list를 가져온다
-start = time.time()
-print("[begin] Topic: %s 으로 consumer가 메시지 받아옴" % (topic_name))
-for message in consumer:
-    print("Partition: %d, Offset: %d, Value: %s" % ( message.partition, message.offset,message.value ))
+topic_name = "input" #꼭 producer파일 확인하기!
+
+while True:
+    consumer = KafkaConsumer(
+                topic_name,
+                bootstrap_servers=[bootstrap_servers])
+
+    for message in consumer: 
+        edit= json.loads(message.value)
+        print(message.topic, message.partition, 
+            message.offset, message.key, edit )
     
-print("[end] 걸린시간 :", time.time() - start)
