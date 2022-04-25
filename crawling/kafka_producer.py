2346a87e51e49f068763373265aa46e44d426e99
EunKyung Ban <eunkyung6969@gmail.com>
2022-04-25 19:27:28 +0900

    final갖고온거
    Former-commit-id: 5986773ecd9acab03fe25e872183759c9262df69

--- crawling/kafka_producer.py
+++ crawling/kafka_producer.py
@@ -2,14 +2,16 @@ from kafka import KafkaProducer
 from json import dumps
 import time
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 bootstrap_servers = os.getenv('KAFKA_SERVER')
 
 topic_name = "test_topic" #토픽확인! 
 producer = KafkaProducer(
         acks=0,
         compression_type='gzip',
-        bootstrap_servers=[bootstrap_servers],
+        bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x: dumps(x).encode('utf-8')
         )
 
