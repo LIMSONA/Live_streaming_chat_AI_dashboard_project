44bb13d7bf5869a927917c8f5afb5cafe0e9b878
LIMSONA <lightsn52@gmail.com>
2022-04-26 16:01:57 +0900

    spark_docker_down.zip
    Former-commit-id: cb77a820ea10da4ed1a8a0ee12e075beed9bad19

---
+++ pipeline/assets/spark/build.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+docker build -t my-spark:0.1 .
+
+
+
---
+++ pipeline/assets/spark/streamingspark.py
@@ -0,0 +1,40 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
+from pyspark.sql.types import StructField
+from pyspark.sql.types import IntegerType
+from pyspark.sql.types import StringType
+
+
+KAFKA_TOPIC = "input"
+KAFKA_SERVER = "localhost:9092"
+
+spark_session = SparkSession \
+    .builder \
+    .appName("kafka-to-spark") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark \
+.readStream\
+.format('kafka') \
+.option('kafka.bootstrap.servers', 'kafka:9092') \
+.option("startingOffsets", "earliest") \
+.option('subscribe', 'input') \
+.load()
+
+df1 = df.selectExpr('CAST(value AS STRING) as value')
+
+schema = StructType([ \
+StructField("video_unique",StringType(),True), \
+StructField("num",StringType(),True), \
+StructField("chat_time",TimestampType(),True), \
+StructField("chat_id",StringType(),True), \
+StructField("chat_message",ArrayType(StringType()),True), \
+])
+
+df2 = df1.select(from_json("value",schema).alias("data")).select("data.*")
+
