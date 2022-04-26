08629babe3e79cd0c31be949ce003c47afb32146
stevensong97 <stsong@iu.edu>
2022-04-26 18:20:43 +0900

    streaming 파일 작성
    Former-commit-id: 9dc455c2168b46791209ad9c6801835dbcec758c

--- pipeline/assets/spark/streamingspark.py
+++ pipeline/assets/spark/streamingspark.py
@@ -1,17 +1,33 @@
+%pyspark
 import pyspark
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark import SparkConf
 from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import desc
+from pyspark.sql.functions import split
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import IntegerType
 from pyspark.sql.types import StringType
+from pyspark.sql import SparkSession
 
+sparkConf = SparkConf()
+conf.setMaster('spark://spark-master:7077')
+conf.setAppName('spark-streaming')
 
-KAFKA_TOPIC = "crawling1"
-KAFKA_SERVER = "localhost:9092"
+spark = SparkSession \
+.builder \
+.config("SparkConf") \
+.getOrCreate()
+
+sparkconf = SparkConf() \
+    .setMaster("spark://spark-master:7077") \
+    .setAppName('spark-basic') \
 
 spark_session = SparkSession \
     .builder \
@@ -23,30 +39,26 @@ df = spark_session \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "json_topic") \
+    .option("subscribe", "input") \
     .option("startingOffsets", "earliest") \
     .load()
 
 df1 = df.selectExpr("CAST(value AS STRING)")
 
 schema = StructType([ \
-StructField("id",IntegerType), \
-StructField("firstname",StringType), \
-StructField("middlename",StringType), \
-StructField("lastname",StringType), \
-StructField("dob_year",IntegerType), \
-StructField("dob_month",IntegerType), \
-StructField("gender",StringType), \
-StructField("salary",IntegerType)
+StructField("video_unique",StringType(),True), \
+StructField("num",StringType(),True), \
+StructField("chat_time",TimestampType(),True), \
+StructField("chat_id",StringType(),True), \
+StructField("chat_message",StringType(),True), 
 ])
 
 df2 = df1.select(from_json("value",schema).alias("data")).select("data.*")
 
-df2 \
-.selectExpr("CAST('id' AS STRING) AS key", "to_json(struct(*)) AS value") \
-.writeStream \
-.format('kafka') \
-.outputMode("append") \
-.option('kafka.bootstrap.servers', 'kafka:9092') \
-.option('topic', 'json_data_topic') \
-.start().awaitTermination()
+df2.writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka:9092") \
+    .option("topic", "output") \
+    .outputMode("complete") \
+    .option("checkpointLocation", "/streaming/checkpointLocation") \
+    .start()
