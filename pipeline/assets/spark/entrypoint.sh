7a35609b6511f080dfcffccc7a6f8063c638289c
LIMSONA <lightsn52@gmail.com>
2022-04-27 13:02:28 +0900

    Revert "통합빌드 작업중 - 로그인 처리 추가 작업 필요"
    This reverts commit c3e3814ca356fd5ecf6cea8383ba2c9c1dbd330d [formerly e06fc10b61b85ff5214c3663b65fe385ec468443].
    
    
    Former-commit-id: 4614717a9bfbb0180ed8344944378805625f86c7

--- pipeline/assets/spark/entrypoint.sh
+++ pipeline/assets/spark/entrypoint.sh
@@ -7,8 +7,3 @@ fi
 if [ "$SPARK_MODE" = "worker" ]; then
     /opt/spark/sbin/start-worker.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}
 fi
-
-if [ "$SPARK_MODE" = "zeppelin" ]; then
-    cd /opt/zeppelin
-    ./bin/zeppelin.sh
-fi
