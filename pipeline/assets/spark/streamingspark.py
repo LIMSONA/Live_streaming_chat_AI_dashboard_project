150b3c715b76c3736413a84080a568ea99b8c231
stevensong97 <stsong@iu.edu>
2022-04-27 09:37:15 +0900

    commit
    Former-commit-id: e573bc89f7722b5a3f585e57a715c85c8fe63896

--- pipeline/assets/spark/streamingspark.py
+++ pipeline/assets/spark/streamingspark.py
@@ -1,5 +1,3 @@
-%pyspark
-import pyspark
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark import SparkConf
@@ -20,14 +18,10 @@ sparkConf = SparkConf()
 conf.setMaster('spark://spark-master:7077')
 conf.setAppName('spark-streaming')
 
-spark = SparkSession \
+spark_session = SparkSession \
 .builder \
 .config("SparkConf") \
 .getOrCreate()
-
-sparkconf = SparkConf() \
-    .setMaster("spark://spark-master:7077") \
-    .setAppName('spark-basic') \
 
 spark_session = SparkSession \
     .builder \
@@ -39,26 +33,29 @@ df = spark_session \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "input") \
+    .option("subscribe", "jason_topic") \
     .option("startingOffsets", "earliest") \
     .load()
-
+    
 df1 = df.selectExpr("CAST(value AS STRING)")
 
 schema = StructType([ \
-StructField("video_unique",StringType(),True), \
-StructField("num",StringType(),True), \
-StructField("chat_time",TimestampType(),True), \
+StructField("video_id",StringType(),True), \
+StructField("user_name",StringType(),True), \
 StructField("chat_id",StringType(),True), \
-StructField("chat_message",StringType(),True), 
+StructField("chat_text",StringType(),True), \
+StructField("noun_token",ArrayType(StringType()),True), \
+StructField("chat_time", TimestampType(),True)
 ])
 
 df2 = df1.select(from_json("value",schema).alias("data")).select("data.*")
 
-df2.writeStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("topic", "output") \
-    .outputMode("complete") \
-    .option("checkpointLocation", "/streaming/checkpointLocation") \
-    .start()
+df2 \
+.selectExpr("CAST('data' AS STRING) AS key", "to_json(struct(*)) AS value") \
+.writeStream    \
+.format('kafka') \
+.option('kafka.bootstrap.servers', 'kafka:9092') \
+.option("topic", "json_data_topic") \
+.option("checkpointLocation", "/streaming/checkpointLocation") \
+.start() 
+.awaitTermination()
