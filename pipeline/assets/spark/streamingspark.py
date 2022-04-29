b6ed8143b13fc7fb626925f228dedb1dea6c9725
stevensong97 <stsong@iu.edu>
2022-04-29 21:30:47 +0900

    spark 스칼라 코드로 카프카 에서 데이터 받기 성공
    Former-commit-id: 75073b9c555e35771776e4f95a703c84f2d6e593

--- pipeline/assets/spark/streamingspark.py
+++ pipeline/assets/spark/streamingspark.py
@@ -1,4 +1,3 @@
-import pyspark
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark import SparkConf
@@ -10,7 +9,7 @@ from pyspark.sql.types import IntegerType
 from pyspark.sql.types import StringType
 
 
-KAFKA_TOPIC = "crawling1"
+KAFKA_TOPIC = "input"
 KAFKA_SERVER = "localhost:9092"
 
 spark_session = SparkSession \
@@ -23,30 +22,27 @@ df = spark_session \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "json_topic") \
+    .option("subscribe", "input") \
     .option("startingOffsets", "earliest") \
     .load()
 
 df1 = df.selectExpr("CAST(value AS STRING)")
 
 schema = StructType([ \
-StructField("id",IntegerType), \
-StructField("firstname",StringType), \
-StructField("middlename",StringType), \
-StructField("lastname",StringType), \
-StructField("dob_year",IntegerType), \
-StructField("dob_month",IntegerType), \
-StructField("gender",StringType), \
-StructField("salary",IntegerType)
+StructField("video_unique",StringType()), \
+StructField("num",IntegerType()), \
+StructField("chat_time",TimestampType()), \
+StructField("chat_id",StringType()), \
+StructField("chat_message",StringType()), \
 ])
 
 df2 = df1.select(from_json("value",schema).alias("data")).select("data.*")
 
-df2 \
-.selectExpr("CAST('id' AS STRING) AS key", "to_json(struct(*)) AS value") \
-.writeStream \
-.format('kafka') \
-.outputMode("append") \
-.option('kafka.bootstrap.servers', 'kafka:9092') \
-.option('topic', 'json_data_topic') \
-.start().awaitTermination()
+# df2 \
+# .selectExpr("CAST('id' AS STRING) AS key", "to_json(struct(*)) AS value") \
+# .writeStream \
+# .format('kafka') \
+# .outputMode("append") \
+# .option('kafka.bootstrap.servers', 'kafka:9092') \
+# .option('topic', 'json_data_topic') \
+# .start().awaitTermination()
