697caa40279d2fc01f8a31eb5f997939856b14de
KaheePark <mydnjang@naver.com>
2022-05-20 17:47:42 +0900

    파이썬 버전 변경, dockerfile , requirement.txt 수정
    Former-commit-id: bc0dc1f4fe7da687e50451243559298d86ccb7d4

--- modeling/run.sh
+++ modeling/run.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-docker run --gpus '"device=0"' -d \
+docker stop jupyter-notebook
+docker rm jupyter-notebook
+
+docker run -it --gpus '"device=0"' \
     -e JUPYTER_PORT=8000 \
     -p 8080:8000 \
     -v ${PWD}/app:/usr/src/app \
     --name=jupyter-notebook \
     my-cuda:0.1
 
+# docker run -it \
+#     -v ${PWD}/app:/usr/src/app \
+#     --name=jupyter-notebook \
+#     my-cuda:0.1
