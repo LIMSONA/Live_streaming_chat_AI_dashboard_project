c28e4d257438fbe8d19e8bb96d58158fad93b1b1
임소나_home <lightsn52@naver.com>
2022-05-28 16:10:59 +0900

    파이썬으로 베이스이미지 변경 및 크롤링기능 폴더 위치 변경(+도커안에서 테스트 필요함)
    Former-commit-id: 5566cb7a31e174ef45f19cfd13dd7918b1cc4d14

--- dashboard_django/fx_django/cr_love.py
+++ dashboard_django/fx_django/cr_love.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup 
-
+import os
+os.environ['WDM_LOG_LEVEL'] = '0' #크롬 시작문구들 안뜨게
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -11,30 +12,30 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
 def youtube_love(video_url):
-    try: 
-        while True:
+    a=0
+    while a==0:
+        try:
             driver.get(url=video_url)
             html = driver.page_source
             soup = BeautifulSoup(html, "html.parser")
             love = driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[8]/div[1]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/div[1]/ytd-toggle-button-renderer[1]/a/yt-formatted-string').text
-            return love        
-    except:
-        return "오류발생"
-    finally:
-            driver.close()
+            print(love)        
+        except:
+            pass
+
         
 def naver_love(video_url): 
-    try:
-        while True:
+    a=0
+    while a==0:
+        try:
+            # print("네이버")
             driver.get(url=video_url)
             html = driver.page_source
             soup = BeautifulSoup(html, "html.parser")
             love = driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div/header/div/a[2]/div/span[3]/span[2]').text
-            return love
-    except:
-        return "오류발생"
-    finally:
-            driver.close()
+            print(love)
+        except:
+            pass
 
 
 want= input()
