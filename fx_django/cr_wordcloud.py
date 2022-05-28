3564984a9e053ee56ff61b18aff0ea991b2bb1f8
임소나_home <lightsn52@naver.com>
2022-05-29 02:26:48 +0900

    워드클라우드 만들어지는 것 확인됨 (마스크이미지, 글씨체, 색깔 등 디자인요소 적용 필요)
    Former-commit-id: 319be7cabe9816a8716df4fa149f5828e9b4bade

--- fx_django/cr_wordcloud.py
+++ fx_django/cr_wordcloud.py
@@ -1,50 +1,39 @@
-# # 0. 맨처음
-# #--> 맨처음 시간을 변수로 지정해놓고 처음 30초동안엔 5초단위로 쪼개서 워드클라우드
-# # 1. 30초동안 작동된다
-# # 2. 코멘트를 입력한다. 
-# #--> df으로 모아놨다가 한번에 빡 리스트화 시키는 방법
-# #--> 코멘트마다 리스트에 추가하는 방법
-# # 3. 명사형으로 변환한다.
 from konlpy.tag import Okt
 from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from PIL import Image
-from datetime import datetime
 import pytchat
 import time
-from datetime import datetime, timedelta
-import pandas as pd
+import datetime 
+import pandas as pdSS
 import os
 import re
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import numpy as np
-from crawling.cr_wordcloud import c_wordcloud
-
-
-# 처음시작시간 찍고
-current = datetime.datetime.now()
-thirty_seconds_later = current + datetime.timedelta(seconds=30)
-
-
-
 
 okt= Okt()    
-bucket=[]
-def crawling(want):
+
+
+def crawling_5(want):
+    current= datetime.datetime.now()
 # 유튜브
     if "youtube" in want:
         print("유튜브")
         video_id= want.split('?v=')[-1]
         chat = pytchat.create(video_id=video_id)
-        while chat.is_alive():
+        while True:
             for c in chat.get().sync_items():
                 re_string= re.sub('[^ A-Za-z0-9ㄱ-ㅣ가-힣]+',"", str(c.message))
                 bucket.append(re_string) #워드클라우드를 위한 리스트 추가
+                print(re_string)
+            if datetime.datetime.now() <= current + datetime.timedelta(seconds=5):
+                break            
+            
 # 네이버
     else:
-        print("네이버")
+        # print("네이버========================================")
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
@@ -55,7 +44,6 @@ def crawling(want):
             driver.get(want)
             pop_list= []
             # 채팅 id와 내용
-            # n_chat_name = driver.find_elements_by_class_name('Comment_id_3pR4u')
             n_chat_message = driver.find_elements_by_class_name('Comment_comment_2d0tc')
             
             for i in range(len(n_chat_message)):
@@ -70,44 +58,98 @@ def crawling(want):
                         print(n_chat_message[i].text)
                 except:
                     pass
-
-
-
+            if datetime.datetime.now() >= current + datetime.timedelta(seconds=5):
+                break
+            
+def crawling_30(want):
+    current= datetime.datetime.now()
+# 유튜브
+    if "youtube" in want:
+        print("유튜브")
+        video_id= want.split('?v=')[-1]
+        chat = pytchat.create(video_id=video_id)
+        while True:
+            for c in chat.get().sync_items():
+                re_string= re.sub('[^ A-Za-z0-9ㄱ-ㅣ가-힣]+',"", str(c.message))
+                bucket.append(re_string) #워드클라우드를 위한 리스트 추가
+                print(re_string)
+            if datetime.datetime.now() <= current + datetime.timedelta(seconds=30):
+                break            
+            
+# 네이버
+    else:
+        # print("네이버========================================")
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_experimental_option("excludeSwitches", ["enable-logging"]) # 실행시 에러메시지 해결
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        while True:
+            driver.get(want)
+            pop_list= []
+            # 채팅 id와 내용
+            n_chat_message = driver.find_elements_by_class_name('Comment_comment_2d0tc')
+            
+            for i in range(len(n_chat_message)):
+                # if n_chat_message[i].text: #채팅이 있는 경우
+                try:
+                    if (n_chat_message[i].text) in pop_list: 
+                        pass
+                    else: #중복되지 않는 경우
+                        pop_list.append(n_chat_message[i].text) #중복을 대조하기 위한 리스트에 추가
+                        re_string= re.sub('[^ A-Za-z0-9ㄱ-ㅣ가-힣]+',"", str(n_chat_message[i].text)) #워드클라우드를 위한 리스트추가
+                        bucket.append(re_string)
+                        print(n_chat_message[i].text)
+                except:
+                    pass
+            if datetime.datetime.now() >= current + datetime.timedelta(seconds=30):
+                break
+ 
+ 
 # 워드클라우드 실행
-# url 받고
-want= input()
-# 채팅 크롤링 시행하고
-crawling(want)
-
-bucket2=[]
-for i in bucket:
-    morph= okt.nouns(i)
-    for j in morph:
-        bucket2.append(j)
+def word_cloud(bucket):
+    bucket2=[]
+    for i in bucket:
+        morph= okt.nouns(i)
+        # print(morph)
+        for j in morph:
+            # print(j)
+            bucket2.append(j)
+    # print(bucket2)
     counts= Counter(bucket2)
-    mask = np.array(Image.open('comments.png'))
-    wc= WordCloud(font_path='C:/Windows/Fonts/BMHANNA_11yrs_ttf.ttf', width=400, height=400, 
-                  scale=2.0, max_font_size=250, background_color="white", mask=mask)
+    # print(counts)
+    # mask = np.array(Image.open('C:\git\\hy22-platform\\fx_django\\comments.png'))
+    wc= WordCloud(font_path='malgun', width=400, height=400, 
+                scale=2.0, max_font_size=250, background_color="white")
     gen= wc.generate_from_frequencies(counts)
     # plt.figure()
     # plt.imshow(gen)
     # 파일명은 날짜와 시간형식으로  
-    time= datetime.now().strftime('%y-%m-%d_%H-%M-%S')
+    time= datetime.datetime.now().strftime('%y-%m-%d_%H-%M-%S')
     # plt.savefig('c_wordcloud_{time}.png')
-    gen.to_file("./imagewordcloud_{}.png".format(time))
+    gen.to_file("C:\git\\hy22-platform\\fx_django\\image\\imagewordcloud_{}.png".format(time))
+
+# url 받고
+want= input()
+# want= "https://shoppinglive.naver.com/lives/541905?fm=shoppinglive&sn=home"
+#현재시간으로 부터 +30초 이전
+#5초동안 크롤링 => 워드클라우드  ---->  총6번
+for i in range(6):
+    bucket=[]
+    crawling_5(want)
+    # print("워드클라우드 여기======================================")
+    # print(bucket)
+    word_cloud(bucket)
 
 
-
-
-
-
-
-# # ★ if <=현재시간이 30초 이전 시점
-# currentTime = datetime.datetime.now()
-# if currentTime<=thirty_seconds_later:
-#     for i in range(5):
-        
-#         c_wordcloud()        
+#현재시간으로부터 +30초 이후인가?
+#30초이후 오류생기지 않는 한 반복 
+#30초동안 크롤링 => 워드클라우드
+while True:
+    bucket=[]
+    crawling_30(want)
+    # print("워드클라우드 여기=================================")
+    word_cloud(bucket)
     
-# else:
     
