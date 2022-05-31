7bcf72306b1d3854cd0e29921a520849926d18da
EunKyung Ban <eunkyung6969@gmail.com>
2022-05-31 19:03:28 +0900

    장고 포트 오픈
    Former-commit-id: 979ff8190254e2dfba1086210f7b04495deaab7d
    Former-commit-id: 3b1f8244b60812bc559f91840274b8f446d6e425
    Former-commit-id: f8b10ed05021119d740f3efd2fc0650df696cae7

--- dashboard_django/mysite/mysite/settings.py
+++ dashboard_django/mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f_@8e6kfdbjrdsj&9pl8y*w64i%l2bklx2arhr_)5(@wg7h9j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0:8000","0.0.0.0"]
 
 
 # Application definition
