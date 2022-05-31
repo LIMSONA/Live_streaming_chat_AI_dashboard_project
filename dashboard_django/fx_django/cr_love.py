dcbda70eb291c4ad6b0c4e05e188ababc99ed4e9
LIMSONA <lightsn52@gmail.com>
2022-05-31 12:05:12 +0900

    크롤링 테스트 모두 완료
    Former-commit-id: af0f699e34381ef63352914442474d8967894645

--- dashboard_django/fx_django/cr_love.py
+++ dashboard_django/fx_django/cr_love.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup 
-import os
-os.environ['WDM_LOG_LEVEL'] = '0' #크롬 시작문구들 안뜨게
+
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -16,8 +15,8 @@ def youtube_love(video_url):
     while a==0:
         try:
             driver.get(url=video_url)
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
+            # html = driver.page_source
+            # soup = BeautifulSoup(html, "html.parser")
             love = driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[8]/div[1]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/div[1]/ytd-toggle-button-renderer[1]/a/yt-formatted-string').text
             print(love)        
         except:
@@ -30,8 +29,8 @@ def naver_love(video_url):
         try:
             # print("네이버")
             driver.get(url=video_url)
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
+            # html = driver.page_source
+            # soup = BeautifulSoup(html, "html.parser")
             love = driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div/header/div/a[2]/div/span[3]/span[2]').text
             print(love)
         except:
--- dashboard_django/fx_django/cr_play.py
+++ dashboard_django/fx_django/cr_play.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime as dt
 from bs4 import BeautifulSoup
-import os
-os.environ['WDM_LOG_LEVEL'] = '0' #크롬 시작문구들 안뜨게
+
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -17,8 +16,8 @@ def youtube_play(video_url):
     while a==0:   
         try:
             driver.get(url=video_url)
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
+            # html = driver.page_source
+            # soup = BeautifulSoup(html, "html.parser")
             play = driver.find_element_by_xpath('//*[@id="count"]/ytd-video-view-count-renderer/span[1]').text
             play2 =str(play).split()[1]
             print(play2)
@@ -33,8 +32,8 @@ def naver_play(video_url):
         try:
             # print("네이버")
             driver.get(url=video_url)
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
+            # html = driver.page_source
+            # soup = BeautifulSoup(html, "html.parser")
             play = driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div/header/div/a[2]/div/span[2]/span[2]').text
             print(play)
         except:
