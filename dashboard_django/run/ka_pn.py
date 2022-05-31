49af865bedf0151095ed4ca559e12154f2123073
EunKyung Ban <eunkyung6969@gmail.com>
2022-05-31 17:01:13 +0900

    test
    Former-commit-id: deb9792cde703a0ba5425dc01f9eb63cd4eb0812

--- dashboard_django/run/ka_pn.py
+++ dashboard_django/run/ka_pn.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 bootstrap_servers="kafka:9092"
 
-topic_name = "message"
+topic_name = "input"
 
 while True:
     consumer = KafkaConsumer(
@@ -23,10 +23,10 @@ while True:
             
             import matplotlib.pyplot as plt
 
-ratio = [34, 32, 16, 18]
-labels = ['Apple', 'Banana', 'Melon', 'Grapes']
-colors = ['#ff9999', '#ffc000', '#8fd9b6', '#d395d0']
-wedgeprops={'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
+# ratio = [34, 32, 16, 18]
+# labels = ['Apple', 'Banana', 'Melon', 'Grapes']
+# colors = ['#ff9999', '#ffc000', '#8fd9b6', '#d395d0']
+# wedgeprops={'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
 
-plt.pie(ratio, labels=labels, autopct='%.1f%%', startangle=260, counterclock=False, colors=colors, wedgeprops=wedgeprops)
-plt.show()
+# plt.pie(ratio, labels=labels, autopct='%.1f%%', startangle=260, counterclock=False, colors=colors, wedgeprops=wedgeprops)
+# plt.show()
