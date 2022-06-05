44bfd00b4bd12577ec32a19773ff9aa901cbc585
EunKyung Ban <eunkyung6969@gmail.com>
2022-06-05 17:03:01 +0900

    startbutton 만드는중
    Former-commit-id: 8af59117f05a5485cf9415a9ffc7d6b61c497ea2
    Former-commit-id: a794393d11a61c673fac754826b938618f0f657e
    Former-commit-id: 7ca1426d6ab1abbc700f17dd3077c6a719c07431

--- dashboard_django/mysite/home/livecommerce.py
+++ dashboard_django/mysite/home/livecommerce.py
@@ -13,11 +13,14 @@ from json import dumps
 import random
 from kafka.admin import KafkaAdminClient, NewTopic
 
+import startbutton
+
 import urllib
 # from cr_kafka import c_kafka
 #
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
