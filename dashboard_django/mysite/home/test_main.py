6b0c1e1b32084c5aa37039402b15411e89537016
EunKyung Ban <eunkyung6969@gmail.com>
2022-06-07 18:19:56 +0900

    복구
    Former-commit-id: 3474b37bfb11063613564ad9cd4e9e6f2dac81a5
    Former-commit-id: 21a9923ae14293ae2b3963942c7639f936003726
    Former-commit-id: 664949fe8b9b4cae63ae945cb6e4213227f447b0

--- dashboard_django/mysite/home/test_main.py
+++ dashboard_django/mysite/home/test_main.py
@@ -4,11 +4,6 @@ from kafka import KafkaProducer, KafkaConsumer
 from json import dumps, loads
 import time
 import os
-from dotenv import load_dotenv
-
-import re
-from konlpy.tag import Okt
-from jamo import h2j, j2hcj #초성/중성/종성분리
 
 import pytchat
 import time
@@ -16,26 +11,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime as dt
 
-import cr_kafka # 카프카 파일명
-import cr_token # 토큰 파일명
-import cr_crawling # 크롤링 파일명
+from home.crawling import cr_kafka # 카프카 파일명
+from home.crawling import cr_token # 토큰 파일명 
+from home.crawling import cr_crawling # 크롤링 파일명 
 
 ck = cr_kafka.c_kafka() # c_kafka 클래스
 ct = cr_token.c_token() # c_token 클래스
-cr = cr_crawling.c_crawling() # crawling 클래스
+cr = cr_crawling.c_crawling() # crawling 클래스 
 
 
 # 카프카 
-kafka_topic= ['input']
+kafka_topic= ['input']  
 
 # 유튜브 크롤링 test
 
-# video_url = str(input())
-video_url= "https://www.youtube.com/watch?v=py_phbQxy5Y"
-if "youtube" in video_url:
-    print("유튜브방송!")
-    cr.youtube_kafka(video_url, kafka_topic[0])
-else:
-    print("네이버방송!")
-    cr.naver_kafka(video_url, kafka_topic[0])
-    
+def startSaveChat(want, video_url):
+    if "youtube" in video_url:
+        print("youtube streaming!")
+        cr.youtube_kafka(video_url, kafka_topic[0])
+    else:
+        print("naver  streaming!")
+        cr.naver_kafka(video_url, kafka_topic[0])
+        
