ceaf30dde2661f44c124dace3bf56884ba7c0485
stevensong97 <stsong@iu.edu>
2022-06-08 16:58:38 +0900

    user agent 수정
    Former-commit-id: 3b53cd8dec5422f197a796cf494bc487913638a7
    Former-commit-id: 22abfcce9d5233c00d5d4083b39339e44c20b1ef
    Former-commit-id: e8771b0a421634fc42a3b32544dd385ffb8f7336

--- modeling/naver_crawling_df.py
+++ modeling/naver_crawling_df.py
@@ -14,7 +14,7 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.add_argument("User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
+options.add_argument("User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko)")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.implicitly_wait(5)
 
