d3f8ee13b13815bf4e7b1f188f8d46af079c965a
stevensong97 <stsong@iu.edu>
2022-06-08 16:56:46 +0900

    또 수정
    Former-commit-id: fb448826da2e007fd216905ba48a1edd0ee8c77e
    Former-commit-id: d7ba10da9ab30975cbad3f66524af0d06a728460
    Former-commit-id: 69b41f7415819a22fbafc04244c48661f33195c3

--- modeling/naver_crawling_df.py
+++ modeling/naver_crawling_df.py
@@ -14,7 +14,7 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+options.add_argument("User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.implicitly_wait(5)
 
