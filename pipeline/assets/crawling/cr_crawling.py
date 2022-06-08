78d530bf180179c109282de89600672f659900bf
임소나_home <lightsn52@naver.com>
2022-06-08 16:22:03 +0900

    옵션 추가
    Former-commit-id: f604eaff3cde9290e5418d60eb6c077ae3e63fe8
    Former-commit-id: f7c4297a5162b18e26b62a886b8700682e51a8f7
    Former-commit-id: a9899ceb94cbf5f40db3b0aeb1d11cdd2bc500d2

--- pipeline/assets/crawling/cr_crawling.py
+++ pipeline/assets/crawling/cr_crawling.py
@@ -57,6 +57,8 @@ class c_crawling:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--user-data-dir=/data/crawling')
         options.add_experimental_option("excludeSwitches", ["enable-logging"]) # 실행시 에러메시지 해결
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         video_id = video_url.split("/")[-1]
