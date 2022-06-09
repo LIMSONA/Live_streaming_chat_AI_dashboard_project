35b78dddd2526b6b11b90b3d28a55f48266139db
KaheePark <mydnjang@naver.com>
2022-06-09 09:51:24 +0900

    수정
    Former-commit-id: 8ad9e21af5c9a60847840ee52a5a3152023e7cb6
    Former-commit-id: e1bac66a1b26cd5aa896027f78d04af2437b6be7
    Former-commit-id: 8c870525acf4bfa7e99a447f2e6ae2f8799b97ad

--- pipeline/assets/model/PN.py
+++ pipeline/assets/model/PN.py
@@ -80,7 +80,7 @@ learning_rate = 5e-5
 # 저장된 가중치 가져오기
 model_pt = BERTClassifier(bertmodel,  dr_rate=0.5)
 model_pt.load_state_dict(torch.load('/spark-work/model/PN.pt'))
-model_pt.to(device)
+# model_pt.to(device)
 
 # tokenizer= get_tokenizer()
 # tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
